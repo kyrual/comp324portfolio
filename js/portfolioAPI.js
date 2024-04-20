6d82b7d650a3018d0023bdbc4b2a59480121b721
rue <rhu1@luc.edu>
2024-04-20 06:31:16 -0500

    github api implemented

---
+++ js/portfolioAPI.js
@@ -0,0 +1,45 @@
+const username = 'kyrual';
+function repoLibrary(username) {
+    // create a variable to hold the `Promise` returned from `fetch`
+    return Promise.resolve(fetch(`https://api.github.com/users/${username}/repos`));
+}
+
+repoLibrary(username)
+    .then(response => response.json()) // parse response into json
+    .then(data => {
+        // update html with data from github
+        for (let i in data) {
+            // Get the ul with id of userRepos
+
+            if (data.message === "Not Found") {
+                let p = document.getElementById('repos');
+
+                // Create variable that will create li's to be added to ul
+                let ul = document.createElement('ul');
+
+                ul.innerHTML = (`
+                <p><strong>No account exists with username:</strong> ${gitHubUsername}</p>`);
+                // Append each li to the ul
+                p.appendChild(ul);
+            } else {
+
+                let p = document.getElementById('repos');
+
+                // Create variable that will create li's to be added to ul
+                let ul = document.createElement('ul');
+
+                // Create the html markup for each li
+                ul.innerHTML = (`
+                <p><strong>Repo:</strong> ${data[i].name}</p><br>
+                <p><strong>Description:</strong> ${data[i].description}</p><br>
+                <p><strong>URL:</strong> <a href="${data[i].html_url}">${data[i].html_url}</a></p>
+            `);
+
+                // Append each li to the ul
+                p.appendChild(ul);
+            }
+        }
+    })
+
+
+
