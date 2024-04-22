d663a0d1a7794cd7599c2efbe259a85323290859
rue <rhu1@luc.edu>
2024-04-22 06:42:08 -0500

    set up db server, revising a few bugs

--- js/portfolioAPI.js
+++ js/portfolioAPI.js
@@ -7,35 +7,28 @@ function repoLibrary(username) {
 repoLibrary(username)
     .then(response => response.json()) // parse response into json
     .then(repo => {
-        // update html with data from github
+
         for (let i in repo) {
-            // Get the ul with id of userRepos
 
             if (repo.message === "Not Found") {
                 let p = document.getElementById('repos');
 
-                // Create variable that will create li's to be added to ul
                 let ul = document.createElement('ul');
 
                 ul.innerHTML = (`
                 <h1>There are no repositories to display, sorry!</h1>`);
-                // Append each li to the ul
+
                 p.appendChild(ul);
             } else {
-
                 let p = document.getElementById('repos');
 
-                // Create variable that will create li's to be added to ul
                 let ul = document.createElement('ul');
 
-                // Create the html markup for each li
                 ul.innerHTML = (`
-                <p><strong>Repo:</strong> <a href="${repo[i].html_url}">${repo[i].name}</a></p><br>
-                <p><strong>Description:</strong> ${repo[i].description}</p><br>
-                <img src="https://api.github.com/users/${username}/${repo[i]}/img/bg.jpg">
+                <p><a href="${repo[i].html_url}">${repo[i].name}</a></p>
+                <p>${repo[i].description}</p>
+                <img src="https://raw.githubusercontent.com/${username}/${repo[i].name}/main/img/bg.jpg">
             `);
-
-                // Append each li to the ul
                 p.appendChild(ul);
             }
         }
